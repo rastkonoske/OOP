99315c7e67fb0869d75875a079e4ea10a2a8d381
mpocekqa <milovanpocek@gmail.com>
2022-11-07 10:45:10 +0100

    Pregled zadataka za tipove promenljivih i unos s tastature

--- src/promenljive/Zadatak5.java
+++ src/promenljive/Zadatak5.java
@@ -5,6 +5,8 @@ public class Zadatak5 {
     public static void main(String[] args){
 
         float a = 4, r = 7;
+        //promenljiva treba da ima intuitivno ime pkv ti je mozda sad jasno ali zamisli za par meseci od danas
+        //povrsinaKvadrata je mnogo intuitivnije nego pkv
         float Pkv = a*a;
         float Pkr = r*r*3.14f;
 
