19d0143cbd2a480bb8fdcf202d4bcafd9a131f3c
rastko.nosovic <rastko.nosovic@vegait.rs>
2022-12-26 15:40:46 +0100

    Komitovano

--- src/finalTask/Main.java
+++ src/finalTask/Main.java
@@ -1,14 +1,39 @@
 package finalTask;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        Car Punto = new Car();
-        Motorcycle Suzuki = new Motorcycle();
-        Truck FAP = new Truck();
-        Vehicle[] array= {Punto, Suzuki, FAP};
-        fullReport(array);
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Please enter the number of vehicles for your report");
+        int numberOfVehicles  = scan.nextInt();
+        Vehicle[] array = new Vehicle[numberOfVehicles];
+        int j=0;
+        while (j<numberOfVehicles) {
+            System.out.println("Please, choose the type of the vehicle. Press 1 for car, press 2 for motorcycle, press 3 for truck");
+            switch (scan.nextInt()){
+                case 1:
+                    Car car = new Car();
+                    array[j]=car;
+                    j++;
+                    break;
+                case 2:
+                    Motorcycle motor = new Motorcycle();
+                    array[j]=motor;
+                    j++;
+                    break;
+                case 3:
+                    Truck truck = new Truck();
+                    array[j]=truck;
+                    j++;
+                    break;
+                default:
+                    System.out.println("Please enter the correct number for choosing vehicle type.");
+            }
+        }
 
+        fullReport(array);
     }
 
     public static void fullReport (Vehicle[] array) {
